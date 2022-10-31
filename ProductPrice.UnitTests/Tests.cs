504a2aab8a426f08c3479387c171890e695d2c2b
Tintin <yuijuhn.ting@titansoft.com.sg>
2022-10-31 23:54:33 +0800

    add test proj

---
+++ ProductPrice.UnitTests/Tests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+using ProductPrice.Database;
+
+namespace ProductPrice.UnitTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Insert()
+        {
+            var toDb = new Product 
+            {
+                Id   = 1,
+                Name = "Yellow Duck",
+                Price  = 1000
+            };
+            using (var db = new JuinjangContext())
+            {
+                db.Products.Add(toDb);
+                var count = db.SaveChanges();
+                Assert.AreEqual(1, count);
+            }
+        }
+    }
+}
